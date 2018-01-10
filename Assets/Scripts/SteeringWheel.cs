47739d669369edeb0b05906119b08e672c5a3e03
Antonio Bottelier <Antonio3338@gmail.com>
2018-01-10 17:04:13 +0100

    Steering wheel grabbing.

--- Assets/Scripts/SteeringWheel.cs
+++ Assets/Scripts/SteeringWheel.cs
@@ -1,5 +1,4 @@
-﻿
-
+﻿using System.Linq;
 using UnityEngine;
 
 namespace Controller
@@ -9,12 +8,59 @@ namespace Controller
     public class SteeringWheel : Grabbable
     {
         public GameObject[] hands;
+        private GrabbingController[] controllers = new GrabbingController[2]; // 0 is left, 1 is right
+
+        public int ControllersGrabbed
+        {
+            get { return controllers.Count(x => x.Controller != null); }
+        }
+        
+        private float radius = 0.3f;
+
+        private void Start()
+        {
+            
+        }
         
         public override void StartGrab(WandController controller)
         {
             controller.HideControllerModel();
             int i = controller.isLeftHand ? 0 : 1;
             hands[i].SetActive(true);
+            controllers[i].Controller = controller;
+            SetPosition(controller.transform.position);
+        }
+
+        Vector3 SetPosition(Vector3 pos)
+        {
+            // map track pos to local space
+            var trackp = transform.InverseTransformPoint(transform.position);
+            // also map current pos to local space.
+            var position = transform.InverseTransformPoint(pos);
+		
+            var a = trackp.x - position.x;
+            var b = trackp.z - position.z;
+            var mag = Mathf.Sqrt(a * a + b * b);
+
+            // the formula is 
+            // C(x,y) = A(x,y) + r * B(x,y) - A(x,y) / √ (Bx - Ax)^2 + (By - Ay)^2
+            // I use z instead of y, though.
+            float cx = position.x + radius * a / mag;
+            float cz = position.z + radius * b / mag;
+
+            return transform.TransformVector(new Vector3(cx, 0, cz));
+        }
+
+        private void Update()
+        {
+            int controllercount = ControllersGrabbed;
+            
+            
+            foreach (var controller in controllers)
+            {
+                if (controller.Controller != null)
+                    controller.SetLastPosition(controller.Position);
+            }
         }
 
         public override void StopGrab(WandController controller)
@@ -22,6 +68,41 @@ namespace Controller
             controller.HideControllerModel(false);
             int i = controller.isLeftHand ? 0 : 1;
             hands[i].SetActive(false);
+            controllers[i].Controller = null;
+        }
+    }
+
+    struct GrabbingController
+    {
+        private WandController controller;
+        private Vector3 lastPosition;
+
+        public Vector3 Position
+        {
+            get { return controller.transform.position; }
+        }
+
+        public WandController Controller
+        {
+            get { return controller; }
+            set { controller = value; }
+        }
+
+        public Vector3 LastPosition
+        {
+            get { return lastPosition; }
+            set { lastPosition = value; }
+        }
+
+        public void SetLastPosition(Vector3 pos)
+        {
+            lastPosition = pos;
+        }
+
+        public GrabbingController(WandController cont)
+        {
+            lastPosition = cont.transform.position;
+            controller = cont;
         }
     }
 }
