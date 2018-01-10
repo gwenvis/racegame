3cdfad84e421b786e51a4586a31264fbab30a830
Wesley Cats <23973@ma-web.nl>
2018-01-10 18:59:04 +0100

    Update

---
+++ Assets/Scripts/TargetCollision.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TargetCollision : MonoBehaviour {
+
+	[SerializeField] private GameObject wall;
+	[SerializeField] private GameObject train;
+
+	private Vector3 trainStartPos;
+
+	public Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+		trainStartPos = train.transform.GetComponent<TrainMovement>().startPos;
+	}
+
+	private void OnTriggerEnter(Collider col)
+	{
+		if (col.transform.name == "Train")
+		{
+			if (transform.position == startPos)
+			{
+				transform.position = trainStartPos;
+			}
+			else if (transform.position == trainStartPos)
+			{
+				transform.position = startPos;
+			}
+
+			train.GetComponent<NavMeshAgent>().SetDestination(transform.position);
+		}
+	}
+}
---
+++ Assets/Scripts/Train/TrainMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TrainMovement : MonoBehaviour {
+
+	[SerializeField] float moveSpeed;
+	[SerializeField] Transform target;
+
+	private NavMeshAgent navAgent;
+
+	public Vector3 startPos;
+
+	// Use this for initialization
+	void Start ()
+	{
+		startPos = transform.position;
+		navAgent = GetComponent<NavMeshAgent>();
+		navAgent.speed = moveSpeed;
+
+		if (target)
+		{
+			navAgent.SetDestination(target.position);
+		}
+		else
+		{
+			target = GetComponent<Transform>();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+	}
+}
